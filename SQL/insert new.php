affd6903a96eb530b5c694973ceef4ecd5b37eb0
aramazanli <82298215+aramazanli@users.noreply.github.com>
2021-04-11 00:45:46 -0400

    Create insert new.php

---
+++ SQL/insert new.php
@@ -0,0 +1,40 @@
+<?php
+
+
+echo'Hi world<br>';
+echo'Mutleq alinacaq<br><br>';
+
+
+
+
+$telebe = 'Fuad';
+$city = 'Gence';
+
+echo $telebe.' yaxshi oglandir ve o '.$city.' sheherinde yashayir<br>';
+
+//if(SHERT){EMR} else{EMR}
+
+$a = 5;
+$b = 5;
+
+if($a==$b)
+{echo'Deyishenler beraberdir<br>';}
+else
+{echo'Deyishenler beraber deyildir<br>';}
+
+
+$hava = 'soyuq';
+
+
+if($hava=='isti')
+{echo'Qesheng hava var men getdim gezmeye';}
+elseif($hava=='soyuq')
+{echo'Hava soyuqdur eynini isti geyin';}
+elseif($hava=='yagmur')
+{echo'Yagish yagir men hec hara getmeyecem';}
+else
+{echo'Goresen hava indi necedir?';}
+
+
+?>
+
